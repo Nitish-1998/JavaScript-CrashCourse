b9e95cff34bd015306fb9240b2e569033dd613c5
Nitish Ojha <nitishojha33@gmail.com>
2020-01-05 16:20:06 +0530

    Challenge 2 Completed

--- Challenge 1 JavaScript/Script.js
+++ Challenge 1 JavaScript/Script.js
@@ -4,7 +4,7 @@ function ageindays()
     let year=prompt("Enter Your Birth Year:-");
     let ageindayss=(2019-year)*365;
     var h1=document.createElement('h1');
-    let textanswer=document.createTextNode('You Are '+ ageindayss +' Years Old.');
+    let textanswer=document.createTextNode('You Are '+ ageindayss +' Days Old.');
     h1.setAttribute('id','ageindayss');
     h1.appendChild(textanswer);
     document.getElementById('flex-box-result').appendChild(h1);
---
+++ Challenge 2 JavaScript/CatScript.js
@@ -0,0 +1,7 @@
+function generate()
+{
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat');
+    image.src="https://media.glamour.com/photos/580e1f6a9ee18bbc22ab57ef/master/w_1600%2Cc_limit/giphy%2520(9).gif";
+    div.appendChild(image);
+}
